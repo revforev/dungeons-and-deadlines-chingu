ed4f3909c5786536a1d2fcf2a61ec49fe0d59075
revforev <dutulescu_andrei@yahoo.com>
2023-11-05 14:14:53 +0000

    final

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function changeBackgroundColor() {
   }
 // =======================================================TOGGLE DROPDOWN====================================
 var coll = document.getElementsByClassName("collapsible");
-var i;
+var i; 
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
